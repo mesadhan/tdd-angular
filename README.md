7be8782c0407f2a94b6eccf0618c4496625e2e32
mesadhan <sadhansarker3@gmail.com>
2019-07-24 12:49:35 +0600

    created two components

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # TddAngular
 
+
+
+ng g c app-header
+ng g c app-homes
+
+ng s
+
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppHomesComponent } from './app-homes/app-homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppHomesComponent
   ],
   imports: [
     BrowserModule
