93e24a5eeeeeb4af2ff4747d911c209cd784ba0a
mesadhan <sadhansarker3@gmail.com>
2019-07-25 15:48:04 +0600

    remove mock data from service

--- src/app/app-homes/data.service.ts
+++ src/app/app-homes/data.service.ts
@@ -8,22 +8,6 @@ export class DataService {
 
   constructor() { }
   getHomeItems$() {
-    return of([
-      {
-        title: 'Trulli',
-        image: 'https://www.w3schools.com/html/pic_trulli.jpg',
-        location: 'USA',
-      },
-      {
-        title: 'Mac Computer',
-        image: 'https://www.w3schools.com/w3images/mac.jpg',
-        location: 'UK',
-      },
-      {
-        title: 'Flowers in Chania',
-        image: 'https://www.w3schools.com/html/img_chania.jpg',
-        location: 'Chania',
-      }
-    ]);
+    return of([]);
   }
 }
